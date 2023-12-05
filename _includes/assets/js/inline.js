3c2e939d5cb24201d98c5547565d6657036e581d
MARCOUX <67022822+bamboounderwear@users.noreply.github.com>
2023-12-05 09:42:16 -0500

    ham

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -1,2 +1,21 @@
 // Add your inline JS here
 
+(() => {
+    const hamburger = document.getElementById("hamburger");
+    const menu = document.getElementById("overlay");
+    let closed = true;
+  
+    const change = () => {
+      if (closed) {
+        hamburger.classList.add("open");
+        menu.classList.add("menu");
+      } else {
+        hamburger.classList.remove("open");
+        menu.classList.remove("menu");
+      }
+      closed = !closed;
+    };
+  
+    hamburger.addEventListener("click", change);
+  })();
+  
