9d9ab7298ecf7efb83d989db46ba74928735c3b5
MARCOUX <67022822+bamboounderwear@users.noreply.github.com>
2023-12-05 10:22:21 -0500

    removed

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -1,10 +1,1 @@
 // Add your inline JS here
-
-function toggleMenu() {
-    var nav = document.querySelector('nav ul');
-    if (nav.style.display === "block") {
-        nav.style.display = "none";
-    } else {
-        nav.style.display = "block";
-    }
-}
