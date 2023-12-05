8dce07f4838b2a29a5aba660697b9492c6a25cc9
MARCOUX <67022822+bamboounderwear@users.noreply.github.com>
2023-12-05 10:17:59 -0500

    js

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -1,1 +1,10 @@
 // Add your inline JS here
+
+function toggleMenu() {
+    var nav = document.querySelector('nav ul');
+    if (nav.style.display === "block") {
+        nav.style.display = "none";
+    } else {
+        nav.style.display = "block";
+    }
+}
