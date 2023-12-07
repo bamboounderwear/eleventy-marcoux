e8fc651eb98c3da0ea648884d3a56996fca7ba04
MARCOUX <67022822+bamboounderwear@users.noreply.github.com>
2023-12-06 21:40:41 -0500

    fixed

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -1,16 +1,1 @@
 // Add your inline JS here
-
-document.addEventListener('DOMContentLoaded', function() {
-    var hamburger = document.getElementById('hamburger-icon');
-    var closeIcon = document.getElementById('close-icon');
-    var menuContainer = document.getElementById('menu-container');
-  
-    hamburger.addEventListener('click', function() {
-      menuContainer.classList.add('show-menu');
-    });
-  
-    closeIcon.addEventListener('click', function() {
-      menuContainer.classList.remove('show-menu');
-    });
-  });
-  
