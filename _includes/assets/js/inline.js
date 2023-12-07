055c1764884ce08a91404e91ef684413c2c13ccb
MARCOUX <67022822+bamboounderwear@users.noreply.github.com>
2023-12-06 21:19:09 -0500

    line

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -1,1 +1,13 @@
 // Add your inline JS here
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const menuContainer = document.getElementById('menu-container');
+
+    menuToggle.addEventListener('click', function() {
+      menuContainer.classList.toggle('menu-visible');
+      menuContainer.classList.toggle('menu-hidden');
+      // Toggle icon from hamburger to close and vice versa
+      this.classList.toggle('open');
+    });
+  });
