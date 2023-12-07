ca3ac137e807d764b518d7886f678149c1920ebf
MARCOUX <67022822+bamboounderwear@users.noreply.github.com>
2023-12-06 21:37:19 -0500

    menu

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -1,12 +1,16 @@
 // Add your inline JS here
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    const menuToggle = document.getElementById('menu-toggle');
-    const menuContainer = document.getElementById('menu-container');
-
-    menuToggle.addEventListener('click', function() {
-      menuContainer.classList.toggle('menu-visible');
-      menuContainer.classList.toggle('menu-hidden');
-      this.classList.toggle('open');
+document.addEventListener('DOMContentLoaded', function() {
+    var hamburger = document.getElementById('hamburger-icon');
+    var closeIcon = document.getElementById('close-icon');
+    var menuContainer = document.getElementById('menu-container');
+  
+    hamburger.addEventListener('click', function() {
+      menuContainer.classList.add('show-menu');
+    });
+  
+    closeIcon.addEventListener('click', function() {
+      menuContainer.classList.remove('show-menu');
     });
   });
+  
