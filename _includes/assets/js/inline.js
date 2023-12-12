610f1dc2e590f48c2558527daa87e727fb7d33ca
MARCOUX <67022822+bamboounderwear@users.noreply.github.com>
2023-12-12 11:24:00 -0500

    hamburger

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -1,12 +1,17 @@
-// Add your inline JS here
-
 document.addEventListener('DOMContentLoaded', function() {
-    var hamburger = document.getElementById('hamburger-btn');
-    var menu = document.getElementById('menu-container');
-    
-    // Adding an event listener to the hamburger button
-    hamburger.addEventListener('click', function() {
-      // Toggle menu visibility
-      menu.style.right = menu.style.right === '0px' ? '-100%' : '0px';
-    }, false);
-  });
+  var hamburger = document.getElementById('hamburger-btn');
+  var menu = document.getElementById('menu-container');
+
+  // Adding an event listener to the hamburger button
+  hamburger.addEventListener('click', function() {
+    // Toggle menu visibility
+    menu.style.right = menu.style.right === '0px' ? '-100%' : '0px';
+
+    // Toggle the icon
+    if (menu.style.right === '0px') {
+      hamburger.innerHTML = '&#x2716;'; // Close icon
+    } else {
+      hamburger.innerHTML = '&#9776;'; // Hamburger icon
+    }
+  }, false);
+});
