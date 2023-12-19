87046e2d25f032d6f64222a140cbfafe41ea8e42
MARCOUX <67022822+bamboounderwear@users.noreply.github.com>
2023-12-19 16:47:38 -0500

    ico

--- .eleventy.js
+++ .eleventy.js
@@ -76,7 +76,7 @@ module.exports = function(eleventyConfig) {
   });
 
   // Don't process folders with static assets e.g. images
-  eleventyConfig.addPassthroughCopy("favicon.png");
+  eleventyConfig.addPassthroughCopy("favicon.ico");
   eleventyConfig.addPassthroughCopy("static/img");
   eleventyConfig.addPassthroughCopy("admin/");
   // We additionally output a copy of our CSS for use in Decap CMS previews
